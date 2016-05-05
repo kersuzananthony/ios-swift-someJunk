8cfb812af13f0d26ea3d58ac8aa103a4ffafea62
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-05-05 14:15:10 +0800

    Creates custom cell and connect outlets

---
+++ someJunk/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ListCell.swift
+//  someJunk
+//
+//  Created by Kersuzan on 05/05/16.
+//  Copyright © 2016 Kersuzan. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailsLabel: UILabel!
+    
+    
+}
--- someJunk/MainVC.swift
+++ someJunk/MainVC.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
     }
 
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
